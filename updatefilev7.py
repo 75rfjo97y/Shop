2d4148efd588eebac2222c78034f7dee57f16c76
75rfjo97y <CrissieNitsch760857@outlook.com>
2026-08-28 12:39:37 +0600

    Change BOT_TOKEN to new value
    Updated the BOT_TOKEN to a new value.

--- updatefilev7.py
+++ updatefilev7.py
@@ -264,7 +264,7 @@ def premiumize_html(text):
 # এখানে আপনার Bot Token এবং Admin ID বসান
 # ============================================================
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8675144844:AAGoJCLCnJF3NaoBxcpqWJDwHo2CKXh8Hb0")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8675144844:AAFGwe5HbA7Qw9k25xNw5SNthAWjzW07gvY")
 
 # আপনার Telegram numeric User ID এখানে বসান
 # একাধিক Admin হলে: {123456789, 987654321}
